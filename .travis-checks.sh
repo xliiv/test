aea3f29e780e6c2d332cf1ba2925501cb1d78c1d
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2015-04-20 11:01:59 +0200

    ..

--- .travis-checks.sh
+++ .travis-checks.sh
@@ -3,9 +3,8 @@
 #TODO:: make params
 # break build if changes.rst not changed
 git fetch git://github.com/xliiv/test.git develop:develop
-if ! git diff --quiet develop  -- CHANGES.rst
-then
-    echo "file not changed: $?"
+if git diff --quiet develop  -- CHANGES.rst; then
+    echo "file not changed: $? -> exit 1"
     exit 1;
 else
     echo "file changed: $?"
